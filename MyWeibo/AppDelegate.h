dd410b8663dbf24ae4a4dc3f0a565f70a30f7ac6
ChenXin <chenxin@ChenXins-Mac.local>
2015-03-02 00:20:53 -0800

    初始化工程

---
+++ MyWeibo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyWeibo
+//
+//  Created by ChenXin on 3/2/15.
+//  Copyright (c) 2015 ChenXin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
