804ddf1dad2bfab9e1ce9d4c258750f2ffdeac0d
ChenXin <chenxin@ChenXins-Mac.local>
2015-03-02 00:22:14 -0800

    初始化工程

---
+++ MyWeibo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyWeibo
+//
+//  Created by ChenXin on 3/2/15.
+//  Copyright (c) 2015 ChenXin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
