5b8e1be0f4a899adbe6945778e332885ed889c96
ChenXin <chenxin@ChenXins-Mac.local>
2015-03-03 21:00:47 -0800

    为底部tabbar添加加号按钮

--- MyWeibo/Classes/Main/View/CustomTabBar.m
+++ MyWeibo/Classes/Main/View/CustomTabBar.m
@@ -10,12 +10,23 @@
 #import "TabBarButton.h"
 
 @interface CustomTabBar ()
-
+//当前选中的按钮
 @property(nonatomic, weak) TabBarButton *selectedButton;
+//加号按钮
+@property(nonatomic, weak) UIButton *addBtn;
+//底部按钮集合
+@property(nonatomic, strong) NSMutableArray *items;
 
 @end
 
 @implementation CustomTabBar
+
+-(NSMutableArray *)items{
+    if (_items==nil) {
+        _items=[NSMutableArray array];
+    }
+    return _items;
+}
 
 - (id)initWithFrame:(CGRect)frame
 {
@@ -25,6 +36,17 @@
         if (!iOS7) {
             self.backgroundColor=[UIColor colorWithPatternImage:[UIImage imageWithName:@"tabbar_background"]];
         }
+        
+        // 添加加号按钮
+        UIButton *addBtn=[UIButton buttonWithType:UIButtonTypeCustom];
+        [addBtn setBackgroundImage:[UIImage imageWithName:@"tabbar_compose_button"] forState:UIControlStateNormal];
+        [addBtn setBackgroundImage:[UIImage imageWithName:@"tabbar_compose_button_highlighted"] forState:UIControlStateHighlighted];
+        [addBtn setImage:[UIImage imageWithName:@"tabbar_compose_icon_add"] forState:UIControlStateNormal];
+        [addBtn setImage:[UIImage imageWithName:@"tabbar_compose_icon_add_highlighted"] forState:UIControlStateHighlighted];
+        //设置按钮的大小和背景图片宽高一致
+        addBtn.bounds=CGRectMake(0, 0, addBtn.currentBackgroundImage.size.width, addBtn.currentBackgroundImage.size.height);
+        [self addSubview:addBtn];
+        self.addBtn=addBtn;
     }
     return self;
 }
@@ -38,11 +60,13 @@
     TabBarButton *button=[[TabBarButton alloc] init];
     button.item=item;
     [self addSubview:button];
+    //添加到集合中
+    [self.items addObject:button];
     
     //为按钮添加点击事件
     [button addTarget:self action:@selector(buttonClick:) forControlEvents:UIControlEventTouchDown];
     
-    if (self.subviews.count==1) {
+    if (self.subviews.count==2) {
         [self buttonClick:button];
     }
 }
@@ -56,9 +80,16 @@
     CGFloat w=self.frame.size.width/self.subviews.count;
     CGFloat h=self.frame.size.height;
     CGFloat y=0;
-    for (int i=0; i<self.subviews.count; i++) {
+    //设置加号按钮在中间
+    self.addBtn.center=CGPointMake(self.frame.size.width/2, h/2);
+    
+    for (int i=0; i<self.items.count; i++) {
         CGFloat x=i*w;
-        UIButton *button=self.subviews[i];
+        UIButton *button=self.items[i];
+        //如果是第三个按钮，挨着加号按钮计算X值
+        if (i>1) {
+            x+=w;
+        }
         button.frame=CGRectMake(x, y, w, h);
         //设置按钮的索引tag，用于后续点击时显示对应的controller
         button.tag=i;
